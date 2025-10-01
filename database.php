438b468e0152802cc4f91d1930e1292c02a9e42b
daicireel-byte <daicireel@gmail.com>
2025-10-01 16:33:33 -0700

    database.php

--- database.php
+++ database.php
@@ -1,22 +1,13 @@
 <?php
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "library";
+    protected $conn;
 
-class DatabaseConnection
-{
-    private $db_host = "localhost";
-    private $db_user = "root";
-    private $db_pass = "";
-    private $db_name = "library";
-
-    protected $connection;
-
-    public function establishConnection()
-    {
-        try {
-            $this->connection = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_pass);
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->connection;
-        } catch (PDOException $e) {
-            die("Database connection failed: " . $e->getMessage());
-        }
+    public function connect() {
+        $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+        return $this->conn;
     }
 }
